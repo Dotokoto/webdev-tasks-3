a531770218d4ef14d3c08ea132d86dcd13d07d4d
Natalia Aksenova <dotokoto@gmail.com>
2016-03-07 15:12:34 +0500

    initial commit

---
+++ test/flow-test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const flow = require('../lib/flow.js');
+
+const fs = require('fs');
+const directory = '../cats';
+
+
+describe('Flow library', function() {
+    describe('Serial', function() {
+
+    });
+    describe('Parallel', function() {
+
+    });
+    describe('Map', function() {
+
+    });
+});
